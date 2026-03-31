13a52d2cb0ee4041dc3f223765517bc83eb2a865
LUKSOAgent <motors.scrims_3d@icloud.com>
2026-03-31 13:07:38 +0200

    fix(P11): bump pragma to ^0.8.27 for LSP7Votes compatibility

--- contracts/src/governance/CouncilTokenLSP7.sol
+++ contracts/src/governance/CouncilTokenLSP7.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.27;
 
 import {Votes} from "@openzeppelin/contracts/governance/utils/Votes.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
